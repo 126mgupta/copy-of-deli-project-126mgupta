954af40dad61622a3f5cead92b08d6d118393cd1
maya <126mgupta@frhsd.com>
2024-04-07 19:25:26 -0400

    Initial commit

---
+++ FILENAME.js
@@ -0,0 +1,17 @@
+function blt() {
+    window.location.href = 'BLT';
+}
+
+function gc() {
+    window.location.href = 'GC';
+}
+
+function rb() {
+    window.location.href = 'RB';
+}
+
+function qs() {
+    window.location.href = 'QS';
+}
+
+
